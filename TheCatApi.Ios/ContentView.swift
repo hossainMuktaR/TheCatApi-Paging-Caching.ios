88ba1c1f9d0307e7ebdb735a94570bf2c494999e
Hossain Muktar <HossainMuktarDev@gmail.com>
2024-08-28 16:31:43 +0600

    init Commit

---
+++ TheCatApi.Ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TheCatApi.Ios
+//
+//  Created by Hossain Muktar on 28/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TheCatApi.Ios/TheCatApi_IosApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheCatApi_IosApp.swift
+//  TheCatApi.Ios
+//
+//  Created by Hossain Muktar on 28/8/24.
+//
+
+import SwiftUI
+
+@main
+struct TheCatApi_IosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
